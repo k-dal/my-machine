2697ac36b31f54c4cdc8f4e3055e84929fe41051
Kevin Dalias <kevin@dalias.me>
2021-07-28 12:36:24 -0700

    New alias, V-R template changes, linter line-length -> 120
    New alias to open dir from terminal; V-R template spacing change; linters flake8 and pylint configured to line-length 120.

--- python/venv_pip_freeze/.zsh
+++ python/venv_pip_freeze/.zsh
@@ -1,6 +1,9 @@
 click==8.0.1
+importlib-metadata==4.6.1
 pyfiglet==0.8.post1
-python-dateutil==2.8.1
+python-dateutil==2.8.2
 shortuuid==1.0.1
 six==1.16.0
 termdown==1.17.0
+typing-extensions==3.10.0.0
+zipp==3.5.0
